477fa0a1ad600cd6e23830954314423740fb29a6
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-04-12 23:34:57 +0300

    Add helper to display toaster, docblocks

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,11 @@
 import {Languages} from './constants';
 import find from 'lodash/find';
+import {toastr} from 'react-redux-toastr';
 
+/**
+ *
+ * @returns {number}
+ */
 export const getDocumentWidth = () => {
   return Math.max(
     document.documentElement['clientWidth'],
@@ -11,6 +16,10 @@ export const getDocumentWidth = () => {
   );
 };
 
+/**
+ *
+ * @returns {*}
+ */
 export const getFoundationBreakpoint = () => {
   const width = getDocumentWidth();
   if (width < 640)
@@ -24,12 +33,31 @@ export const getFoundationBreakpoint = () => {
   return 'xxlarge';
 };
 
+/**
+ *
+ * @param language
+ * @returns {boolean}
+ */
 export const isAllowedLanguage = (language) => {
   return !!find(Languages, (item) => {
     return language === item;
   });
 };
 
+/**
+ *
+ * @param title
+ */
 export const setPageTitle = (title) => {
   document.title = `${title}`;
 };
+
+/**
+ * Display message in UI
+ * @param message
+ * @param opts
+ */
+export function displayUIMessage(message, opts = {type: 'success'}) {
+  const {title, body} = message;
+  return toastr[opts.type](title, body, opts);
+}
