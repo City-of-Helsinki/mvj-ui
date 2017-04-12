6bae690f8715e7952863da637b3b9e3472d44441
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-04-13 02:29:29 +0300

    Add dummy-form to applicationList-component

--- src/application/ApplicationList.js
+++ src/application/ApplicationList.js
@@ -1,15 +1,68 @@
+// @flow
 import React, {Component} from 'react';
+import {reduxForm, Field} from 'redux-form';
+import flowRight from 'lodash/flowRight';
+
+import FieldTextInput from '../form/field-text-input';
+import {BaseValidator} from '../form/validation';
+
+type Props = {
+  handleSubmit: Function,
+  invalid: Boolean,
+  onCancel: Function,
+  onSave: Function,
+  pristine: Boolean,
+  submitting: Boolean,
+};
+
+const validate = ({test}) => {
+  return BaseValidator({test}, {});
+};
 
 class ApplicationList extends Component {
-  static propTypes = {};
+  props: Props;
+
+  save = (values) => {
+    console.log('saving', values);
+  };
 
   render() {
+    const {
+      handleSubmit,
+      invalid,
+      pristine,
+      submitting,
+    } = this.props;
+
     return (
       <div className="section__container">
-        <p>Applicationlist</p>
+        <form className="test-form" onSubmit={handleSubmit(this.save)}>
+
+          <Field
+            type="text"
+            name="test"
+            placeholder="Test placeholder"
+            component={FieldTextInput}
+            label='Test field'
+            hint='Lorem ipsum color.'
+            disabled={false}
+            required={true}/>
+
+          <button
+            type="submit"
+            className="button"
+            disabled={invalid || submitting || pristine}>
+            Submit-label
+          </button>
+        </form>
       </div>
     );
   }
 }
 
-export default ApplicationList;
+export default flowRight(
+  reduxForm({
+    form: 'testForm',
+    validate,
+  })
+)(ApplicationList);
--- src/form/validation.js
+++ src/form/validation.js
@@ -29,4 +29,4 @@ export const BaseValidator = (fields, customConditions, requiredTexts = {}) => {
   }
 
   return errors;
-}
+};
