6fcf4d36418b491dd13dc69712d17637cd2bf36d
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-04-12 23:34:26 +0300

    Add roles-constant for now

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
+// Langs
 export const AvailableLanguages = [
   'en',
   'fi',
@@ -8,4 +9,11 @@ export const FallbackLanguage = 'en';
 export const Languages = [
   {text: 'EN', key: 'en'},
   {text: 'FI', key: 'fi'},
+];
+
+// Roles
+export const Roles = [
+  {id: 1, label: 'Hakija', name: 'Ville Hakija'},
+  {id: 2, label: 'Hakemuksen käsittelijä', name: 'Ville Hakemuskäsittelijä'},
+  {id: 3, label: 'Valmistelija', name: 'Ville Valmistelija'},
 ];
