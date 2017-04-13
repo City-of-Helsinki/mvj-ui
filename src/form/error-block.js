0a454890ec502c3c8b80c2817870c4376ec501c3
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-04-13 09:45:03 +0300

    Add minor transitions to errorBlock

--- src/form/error-block.js
+++ src/form/error-block.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import {translate} from 'react-i18next';
 
 type Props = {
@@ -7,6 +8,14 @@ type Props = {
   t: Function,
 };
 
-const ErrorBlock = ({error, t}: Props) => <span className="form-field__error-block" role="alert">{t(error)}</span>;
+const ErrorBlock = ({error, t}: Props) =>
+  <CSSTransitionGroup
+    transitionName="error-block"
+    transitionAppear={true}
+    transitionAppearTimeout={0}
+    transitionEnter={false}
+    transitionLeave={false}>
+    <span className="form-field__error-block" role="alert">{t(error)}</span>
+  </CSSTransitionGroup>;
 
 export default translate(['validation'])(ErrorBlock);
