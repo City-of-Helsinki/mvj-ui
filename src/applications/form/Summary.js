d7ef0d817e43d1e0d6050bd467578a554f96dfd9
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-05-08 21:27:42 +0300

    Add lease-code to Summary

--- src/applications/form/Summary.js
+++ src/applications/form/Summary.js
@@ -1,19 +1,63 @@
 // @flow
 import React from 'react';
-import {Row} from 'react-foundation';
+import {Field} from 'redux-form';
+import {Row, Column} from 'react-foundation';
 
-import GroupTitle from '../../components/form/GroupTitle';
-
+import FormField from '../../components/form/FormField';
 // type Props = {};
 
 const Summary = () => {
 
   return (
-    <Row>
+    <div>
 
-      <GroupTitle text="Yhteenveto"/>
+      <section className="summary__header">
+        <Row>
+          <Column medium={6}>
+            <h2>Tallenna hakemus valmisteltavaksi</h2>
+            <p>Tarkista hakijan tiedot ja määritä hakijalle sopiva kohde. Luo vuokraustunnus ja tallenna hakemus
+              valmisteltavaksi.</p>
+          </Column>
 
-    </Row>
+          <Column medium={6} className="lease">
+            <label>Vuokraustunnus</label>
+            <div className="lease__code">
+              <Field
+                type="text"
+                required={false}
+                name="a1"
+                placeholder="A1"
+                component={FormField}
+              />
+              <Field
+                type="text"
+                required={false}
+                name="b1"
+                placeholder="B1"
+                component={FormField}
+              />
+              <Field
+                type="text"
+                required={false}
+                name="c1"
+                placeholder="C1"
+                component={FormField}
+              />
+              <Field
+                type="text"
+                required={false}
+                name="d1"
+                placeholder="D1"
+                component={FormField}
+              />
+            </div>
+
+            <button className="button expanded">Tallenna hakemus valmisteltavaksi</button>
+          </Column>
+        </Row>
+      </section>
+
+    </div>
   );
 };
 
