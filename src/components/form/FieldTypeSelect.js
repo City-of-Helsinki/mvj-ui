6d214b1fc0013f9f3161c7ffb036af91e32b2708
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-05-15 11:49:27 +0300

    Style select2

--- src/components/form/FieldTypeSelect.js
+++ src/components/form/FieldTypeSelect.js
@@ -24,6 +24,7 @@ const FieldTypeSelect = ({input, displayError, disabled, options, placeholder, t
       options={options}
       placeholder={placeholder}
       noResultsText={t('noResultsFound')}
+      valueKey="id"
       {...input}
     />
   );
