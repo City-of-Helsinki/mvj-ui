ef078c05b77516f6919d4c8919abd987df28ec54
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-05-15 11:14:24 +0300

    Add react-select, locales, base-styling & use react-select in place of basic selects

--- src/components/form/FieldTypeSelect.js
+++ src/components/form/FieldTypeSelect.js
@@ -1,6 +1,8 @@
 // @flow
 import React from 'react';
 import classNames from 'classnames';
+import Select from 'react-select';
+import {translate} from 'react-i18next';
 
 type Props = {
   className: String,
@@ -9,19 +11,22 @@ type Props = {
   input: Object,
   options: ?Array<any>,
   placeholder: String,
+  t: Function,
 }
 
-const FieldTypeSelect = ({input, displayError, disabled, options, placeholder}: Props) => {
+const FieldTypeSelect = ({input, displayError, disabled, options, placeholder, t}: Props) => {
   return (
-    <select
+    <Select
+      autoBlur={true}
       id={input.name}
       disabled={disabled}
       className={classNames('form-field__select', {'has-error': displayError})}
-      {...input}>
-      <option value=''>{placeholder}</option>
-      {options && options.map((option) => <option key={option.id} value={option.id}>{option.label}</option>)}
-    </select>
+      options={options}
+      placeholder={placeholder}
+      noResultsText={t('noResultsFound')}
+      {...input}
+    />
   );
 };
 
-export default FieldTypeSelect;
+export default translate(['validation'])(FieldTypeSelect);
