939d5691e680f3d8594f9a1558f31b52b9c3c8c1
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-05-16 14:09:19 +0300

    Add basic editModal

---
+++ src/applications/components/formSections/editModal.js
@@ -0,0 +1,25 @@
+// @flow
+
+import React, {createElement} from 'react';
+import flowRight from 'lodash/flowRight';
+import {translate} from 'react-i18next';
+import {reveal} from '../../../foundation/reveal';
+
+type Props = {
+  component: Object,
+  handleDismiss: Function,
+  handleSave: Function,
+  t: Function,
+};
+
+const EditModal = ({component, handleDismiss, handleSave, t}: Props) => (
+  <div className="edit-modal">
+    <span className="edit-modal__close" onClick={handleDismiss}>{t('close')}</span>
+    {component && createElement(component, {handleSave})}
+  </div>
+);
+
+export default flowRight(
+  reveal({name: 'editModal'}),
+  translate(['common']),
+)(EditModal);
