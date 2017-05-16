6ebc050b723cf7e6a673bf5268d2b3abe9697d67
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-05-16 16:28:52 +0300

    Add Lease-section

--- src/applications/components/formSections/Lease.js
+++ src/applications/components/formSections/Lease.js
@@ -1,18 +1,58 @@
 // @flow
 import React from 'react';
-import {Row} from 'react-foundation';
+import {Column, Row} from 'react-foundation';
 
-import GroupTitle from '../../../components/form/GroupTitle';
+// import GroupTitle from '../../../components/form/GroupTitle';
+import Table from '../../../components/table/Table';
 
-// type Props = {};
+type Props = Object;
 
-const Lease = () => {
+const Lease = (props: Props) => {
 
   return (
-    <Row>
+    <Row className={props.className}>
 
-      <GroupTitle text="Vuokra"/>
+      <Column medium={12}>
+        <h1 className="tab__content--title">Vuokra</h1>
+      </Column>
 
+      <section className="data-box">
+        <div className="data-box__header">
+          <h2>Vuokra <span className="identifier">Tarkentava tieto</span></h2>
+          <div className="data-box__header--item">
+            <span className="identifier">Käyttötarkoitus</span>
+            Asunto
+          </div>
+          <div className="data-box__header--item">
+            <span className="identifier">Sopimusvuokra</span>
+            8920,50€/v
+          </div>
+        </div>
+
+        <div className="data-box__content">
+          <div className="data-box__content--section">
+            <h3>Alennukset ja korotukset</h3>
+
+            <Table
+              headers={[
+                'Tunnus',
+                'Kommentti',
+                'Määrä',
+                'Voimassa',
+              ]}
+              data={[
+                {id: 1, data: {tunnus: 'Alennus', kommentti: 'ARAVA-alennus', maara: '30%', state: '31.1.2017 - 31.12.2018'}},
+              ]}
+            />
+          </div>
+
+          <div className="data-box__controls">
+            <span className="edit">Muokkaa</span>
+          </div>
+        </div>
+      </section>
+
+      <button className="add-new-button">Lisää vuokra</button>
     </Row>
   );
 };
