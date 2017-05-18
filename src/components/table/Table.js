7091659f5073d59cbc33cef447a68c439315448f
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-05-18 10:42:27 +0300

    Refactor table data to be more sane

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,12 +1,14 @@
 // @flow
 import React, {Component} from 'react';
 import classnames from 'classnames';
+import get from 'lodash/get';
 import kebabCase from 'lodash/kebabCase';
 
 type Props = {
   className?: string,
   onRowClick?: Function,
   data: Array<any>,
+  dataKeys: Array<any>,
   headers: Array<any>,
 };
 
@@ -18,7 +20,7 @@ class Table extends Component {
   };
 
   render() {
-    const {className, data, headers, onRowClick} = this.props;
+    const {className, data, dataKeys, headers, onRowClick} = this.props;
 
     return (
       <table className={classnames(className)}>
@@ -30,10 +32,10 @@ class Table extends Component {
         </thead>
         }
         <tbody>
-        {data.map((row) => (
-          <tr key={row.id} onClick={() => onRowClick && onRowClick(row.id)}>
-            {Object.keys(row.data).map((tr, cellIndex) => (
-              <td key={cellIndex}>{row.data[tr]}</td>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex} onClick={() => onRowClick && onRowClick(row.id)}>
+            {dataKeys.map((key, cellIndex) => (
+              <td key={cellIndex}>{get(row, key)}</td>
             ))}
           </tr>
         ))}
