49fae3dd16f60b6b441397cf1861f6ce41a3fecb
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-05-31 12:00:57 +0300

    Add sidebar component

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+// @flow
+import React, {Component, createElement} from 'react';
+import classnames from 'classnames';
+
+type Props = {
+  className: string,
+  component: Object,
+  handleClose: Function,
+  isOpen: boolean,
+  position: string,
+};
+
+class Sidebar extends Component {
+  props: Props;
+
+  render() {
+    const {isOpen, handleClose, className, component, position, ...rest} = this.props;
+
+    return (
+      <div className={classnames(
+        'mvj__sidebar',
+        'off-canvas is-transition-overlap',
+        `position-${position}`,
+        className,
+        {'is-open': isOpen}
+      )}
+           aria-hidden={!isOpen}>
+        <span className="mvj__sidebar__close" onClick={handleClose}>
+          <i className="mi mi-close"/>
+        </span>
+
+        <div className="mvj__sidebar__content">
+          {createElement(component, {...rest})}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
