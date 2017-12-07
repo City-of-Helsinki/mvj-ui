0189681fd0d294ed4a221a5b16867e9b4c36d902
jorilindell <jori.lindell@gmail.com>
2017-12-07 10:43:38 +0200

    Removed Alpha from routing

--- src/root/routes.js
+++ src/root/routes.js
@@ -1,20 +1,9 @@
 // @flow
-
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
 import ErrorPage from '../errorPage/ErrorPage';
-import AppAlpha from '../app-alpha/App';
 import App from '../app/App';
-import i18n from './i18n';
-
-import RoleSelector from '../role/RoleSelector';
-
-import ApplicationsList from '../applications-alpha/components/ApplicationsList';
-import CreateApplicationForm from '../applications-alpha/components/NewApplicationForm';
-
-import LeaseListAlpha from '../leases-alpha/components/LeaseList';
-import PreparerForm from '../leases-alpha/components/PreparerForm';
 import LeaseList from '../leases/components/LeaseList';
 import LeasePage from '../leases/components/LeasePage';
 
@@ -23,22 +12,6 @@ export default
   <IndexRoute onEnter={(nextState, replace) => {
     replace(`/beta`);
   }}/>
-  <Route path="/alpha" component={AppAlpha}>
-    <Route path=":language">
-      <IndexRoute component={(props) => {
-        return <RoleSelector {...props} path={`/alpha/${i18n.language}`}/>;
-      }}/>
-      <Route path="applications">
-        <IndexRoute component={ApplicationsList}/>
-        <Route path="create" component={CreateApplicationForm}/>
-      </Route>
-      <Route path="leases">
-        <IndexRoute component={LeaseListAlpha}/>
-        <Route path=":leaseId" component={PreparerForm}/>
-      </Route>
-      <Route path="*" component={ErrorPage}/>
-    </Route>
-  </Route>
   <Route path="/beta" component={App}>
     <IndexRoute onEnter={(nextState, replace) => {
       replace(`/beta/leases`);
@@ -47,5 +20,6 @@ export default
       <IndexRoute component={LeaseList}/>
       <Route path=":leaseId" component={LeasePage}/>
     </Route>
+    <Route path="*" component={ErrorPage}/>
   </Route>
 </Route>;
