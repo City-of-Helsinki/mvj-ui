eb0b4d081819b4a1594c404d8f3cf5d1804c6637
Jori Lindell <jori.lindell@digia.com>
2017-12-21 15:25:02 +0200

    Fixed lease identifier fields

--- src/leases/components/leaseSections/propertyUnit/PropertyUnit.js
+++ src/leases/components/leaseSections/propertyUnit/PropertyUnit.js
@@ -21,7 +21,7 @@ const PropertyUnit = ({areas}: Props) => {
                 <svg className='map-icon' viewBox="0 0 30 30">
                   <path d="M28.5 2.06v21.52l-.7.28-7.88 3.37-.42.22-.42-.15-8.58-3.23-7.45 3.16-1.55.71V6.42l.7-.28 7.88-3.37.42-.22.42.15 8.58 3.23L27 2.77zM9.38 5.44L3.75 7.83v16.73l5.63-2.39zm2.24-.07V22.1l6.76 2.53V7.9zm14.63.07l-5.63 2.39v16.73l5.63-2.39z"/>
                 </svg>
-                <span>{`${capitalize('Vuokrakohde')} ${area.municipality}-${area.district}-${area.sequence}`}</span>
+                <span>{`${capitalize('Vuokrakohde')} ${area.municipality}-${area.district}-${area.group_number}-${area.unit_number}`}</span>
               </Column>
               <Column medium={4} className='collapse__header-subtitle'>
                 <span>{`${capitalize(area.address)}, ${area.zip_code} ${capitalize(area.town)}`}</span>
--- src/leases/components/leaseSections/propertyUnit/PropertyUnitPlanPlotItem.js
+++ src/leases/components/leaseSections/propertyUnit/PropertyUnitPlanPlotItem.js
@@ -19,7 +19,7 @@ const PropertyUnitPlanPlotItem = (props: Props) => {
           <svg className='map-icon-smaller' viewBox="0 0 30 30">
             <path d="M28.5 2.06v21.52l-.7.28-7.88 3.37-.42.22-.42-.15-8.58-3.23-7.45 3.16-1.55.71V6.42l.7-.28 7.88-3.37.42-.22.42.15 8.58 3.23L27 2.77zM9.38 5.44L3.75 7.83v16.73l5.63-2.39zm2.24-.07V22.1l6.76 2.53V7.9zm14.63.07l-5.63 2.39v16.73l5.63-2.39z"/>
           </svg>
-          <p className='section-item__subtitle'>{`${capitalize(item.explanation)} ${item.municipality}-${item.district}-${item.sequence}`}</p>
+          <p className='section-item__subtitle'>{`${capitalize(item.explanation)} ${item.municipality}-${item.district}-${item.group_number}-${item.unit_number}`}</p>
         </Column>
       </Row>
       <Row>
