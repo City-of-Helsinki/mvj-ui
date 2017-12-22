6f76eb1a0a6f0c98c54253549ec7b0eac1a15e13
Eelsie <elsie@iki.fi>
2017-12-22 10:45:07 +0200

    removed get and added check if contract.pledge_books is null

--- src/leases/components/leaseSections/contract/Contract.js
+++ src/leases/components/leaseSections/contract/Contract.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import get from 'lodash/get';
 import {Row, Column} from 'react-foundation';
 import * as helpers from '../../../../util/helpers';
 
@@ -26,21 +25,21 @@ const Contract = ({contract}: Props) => {
           <Row>
             <Column medium={4}>
               <label>Sopimusnumero</label>
-              <p>{contract.contract_number ? get(contract, 'contract_number', '–') : '–'}</p>
+              <p>{contract.contract_number}</p>
             </Column>
             <Column medium={4}>
               <label>Allekirjoituspäivämäärä</label>
-              <p>{contract.signing_date ? helpers.formatDate(get(contract, 'signing_date', '–')) : '–'}</p>
+              <p>{contract.signing_date ? helpers.formatDate(contract.signing_date) : '–'}</p>
             </Column>
             <Column medium={4}>
               <label>Kommentti allekirjoitukselle</label>
-              <p>{contract.signing_date_comment ? get(contract, 'signing_date_comment', '–') : '–'}</p>
+              <p>{contract.signing_date_comment ? contract.signing_date_comment : '–'}</p>
             </Column>
           </Row>
           <Row>
             <Column medium={4}>
               <label>Järjestelypäätös</label>
-              <p>{contract.setup_decision ? get(contract, 'setup_decision', '–') : '–'}</p>
+              <p>{contract.setup_decision ? contract.setup_decision : '–'}</p>
             </Column>
             <Column medium={4}>
               <label>Päätös</label>
@@ -56,43 +55,43 @@ const Contract = ({contract}: Props) => {
           <Row>
             <Column medium={4}>
               <label>Vuokravakuusnumero</label>
-              <p>{contract.lease_deposit_number ? get(contract, 'lease_deposit_number', '–') : '–'}</p>
+              <p>{contract.lease_deposit_number ? contract.lease_deposit_number : '–'}</p>
             </Column>
             <Column medium={4}>
               <label>Vuokravakuus alku- ja loppupäivämäärä</label>
-              <p>{helpers.formatDate(get(contract, 'lease_deposit_starting_date', '–'))} – {helpers.formatDate(get(contract, 'lease_deposit_ending_date', '–'))}</p>
+              <p>{helpers.formatDate(contract.lease_deposit_starting_date)} – {helpers.formatDate(contract.lease_deposit_ending_date)}</p>
             </Column>
             <Column medium={4}>
               <label>Kommentti</label>
-              <p>{contract.lease_deposit_comment ? get(contract, 'lease_deposit_comment', '–') : '–'}</p>
+              <p>{contract.lease_deposit_comment ? contract.lease_deposit_comment : '–'}</p>
             </Column>
           </Row>
           <Row>
             <Column medium={4}>
               <label>Laitostunnus</label>
-              <p>{contract.administration_number ? get(contract, 'administration_number', '–') : '–'}</p>
+              <p>{contract.administration_number ? contract.administration_number : '–'}</p>
             </Column>
           </Row>
-          {contract.pledge_books.length > 0 &&
+          {contract.pledge_books && contract.pledge_books.length > 0 &&
           <Row>
             <Column>
               <p className='green-subtitle'>Panttikirjat</p>
             </Column>
           </Row>
           }
-          {contract.pledge_books.length > 0 && contract.pledge_books.map((pledge_book, index) =>
+          {contract.pledge_books && contract.pledge_books.length > 0 && contract.pledge_books.map((pledge_book, index) =>
             <Row key={index}>
               <Column medium={4}>
                 <label>Panttikirjan numero</label>
-                <p>{pledge_book.pledge_book_number ? get(pledge_book, 'pledge_book_number', '–') : '–'}</p>
+                <p>{pledge_book.pledge_book_number ? pledge_book.pledge_book_number : '–'}</p>
               </Column>
               <Column medium={4}>
                 <label>Panttikirjan päivämäärä</label>
-                <p>{pledge_book.pledge_book_date ? helpers.formatDate(get(pledge_book, 'pledge_book_date', '–')) : '–'}</p>
+                <p>{pledge_book.pledge_book_date ? helpers.formatDate(pledge_book.pledge_book_date) : '–'}</p>
               </Column>
               <Column medium={4}>
                 <label>Panttikirjan kommentti</label>
-                <p>{pledge_book.pledge_book_comment ? get(pledge_book, 'pledge_book_comment', '–') : '–'}</p>
+                <p>{pledge_book.pledge_book_comment ? pledge_book.pledge_book_comment : '–'}</p>
               </Column>
             </Row>
           )}
@@ -114,30 +113,30 @@ const Contract = ({contract}: Props) => {
             <Row>
               <Column medium={4}>
                 <label>Allekirjoituspäivämäärä</label>
-                <p>{modification.modification_signing_date ? helpers.formatDate(get(modification, 'modification_signing_date', '–')) : '–'}</p>
+                <p>{modification.modification_signing_date ? helpers.formatDate(modification.modification_signing_date) : '–'}</p>
 
                 <label>1. kutsu lähetetty</label>
-                <p>{modification.first_call_sent ? helpers.formatDate(get(modification, 'first_call_sent', '–')) : '–'}</p>
+                <p>{modification.first_call_sent ? helpers.formatDate(modification.first_call_sent) : '–'}</p>
               </Column>
               <Column medium={4}>
                 <label>Allekirjoitettava mennessä</label>
-                <p>{modification.to_be_signed_by ? helpers.formatDate(get(modification, 'to_be_signed_by', '–')) : '–'}</p>
+                <p>{modification.to_be_signed_by ? helpers.formatDate(modification.to_be_signed_by) : '–'}</p>
 
                 <label>2. kutsu lähetetty</label>
-                <p>{modification.second_call_sent ? helpers.formatDate(get(modification, 'second_call_sent', '–')) : '–'}</p>
+                <p>{modification.second_call_sent ? helpers.formatDate(modification.second_call_sent) : '–'}</p>
               </Column>
               <Column medium={4}>
                 <label>Päätös</label>
                 <p>–</p>
 
                 <label>3. kutsu lähetetty</label>
-                <p>{modification.third_call_sent ? helpers.formatDate(get(modification, 'third_call_sent', '–')) : '–'}</p>
+                <p>{modification.third_call_sent ? helpers.formatDate(modification.third_call_sent) : '–'}</p>
               </Column>
             </Row>
             <Row>
               <Column medium={12}>
                 <label>Selite</label>
-                <p>{modification.modification_description ? get(modification, 'modification_description', '–') : '–'}</p>
+                <p>{modification.modification_description ? modification.modification_description : '–'}</p>
               </Column>
             </Row>
           </div>)
