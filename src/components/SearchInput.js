7828648ca6df1928b7e280eefeb81a22cb4aac41
Jori Lindell <jori.lindell@digia.com>
2018-01-10 13:47:39 +0200

    Added search icon for the search field

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -5,22 +5,26 @@ type Props = {
   disabled?: boolean,
   onChange: Function,
   onKeyUp: Function,
+  onSubmit: Function,
   placeholder?: string,
   type?: string,
   value: string,
 }
 
-const SearchInput = ({disabled, onChange, onKeyUp, placeholder = 'Hae hakusanalle', type = 'text', value = ''}: Props) => {
+const SearchInput = ({disabled, onChange, onKeyUp, onSubmit, placeholder = 'Hae hakusanalla', type = 'text', value = ''}: Props) => {
   return (
-    <input
-      className='search-input'
-      disabled={disabled}
-      onChange={onChange}
-      onKeyUp={onKeyUp}
-      placeholder={placeholder}
-      type={type}
-      value={value}
-    />
+    <div className="search-input__component">
+      <input
+        className='search-input'
+        disabled={disabled}
+        onChange={onChange}
+        onKeyUp={onKeyUp}
+        placeholder={placeholder}
+        type={type}
+        value={value}
+      />
+      <span className="search-icon" onClick={onSubmit}></span>
+    </div>
   );
 };
 
