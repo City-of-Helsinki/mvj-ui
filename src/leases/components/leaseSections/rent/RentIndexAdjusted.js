9faedf5afaef78ee151f83e365ca79d2ee9817aa
Jori Lindell <jori.lindell@digia.com>
2018-01-24 15:35:18 +0200

    Styled index adjusted rents table and created component to show table with fixed header

--- src/leases/components/leaseSections/rent/RentIndexAdjusted.js
+++ src/leases/components/leaseSections/rent/RentIndexAdjusted.js
@@ -2,36 +2,41 @@
 import React from 'react';
 
 import {formatDateRange, formatNumberWithThousandSeparator, formatDecimalNumbers} from '../../../../util/helpers';
+import TableFixedHeader from '../../../../components/TableFixedHeader';
+
 type Props = {
   indexAdjustedRents: Array<Object>,
 }
 
+const getTableBody = (indexAdjustedRents: Array<Object>) => {
+  if(indexAdjustedRents && indexAdjustedRents.length > 0) {
+    return indexAdjustedRents.map((rent, index) => {
+      return (
+        <tr key={index}>
+          <td>{rent.rent ? formatNumberWithThousandSeparator(formatDecimalNumbers(rent.rent), '.') : '-'}</td>
+          <td>{rent.purpose ? rent.purpose : '-'}</td>
+          <td>{formatDateRange(rent.start_date, rent.end_date)}</td>
+          <td>{rent.calculation_factor ? formatDecimalNumbers(rent.calculation_factor) : '-'}</td>
+        </tr>
+      );
+    });
+  }
+  else {
+    return null;
+  }
+};
+
 const RentIndexAdjusted = ({indexAdjustedRents}: Props) => {
   return (
-    <div>
-      <table>
-          <thead>
-            <tr>
-              <th>Ind. tark. vuokar (€)</th>
-              <th>Käyttötarkoitus</th>
-              <th>Voimassaoloaika</th>
-              <th>Laskentakerroin</th>
-            </tr>
-          </thead>
-          <tbody>
-            {indexAdjustedRents && indexAdjustedRents.length > 0 && indexAdjustedRents.map((rent) => {
-              return (
-                <tr>
-                  <td>{rent.rent ? formatNumberWithThousandSeparator(formatDecimalNumbers(rent.rent), '.') : '-'}</td>
-                  <td>{rent.purpose ? rent.purpose : '-'}</td>
-                  <td>{formatDateRange(rent.start_date, rent.end_date)}</td>
-                  <td>{rent.calculation_factor ? formatDecimalNumbers(rent.calculation_factor) : '-'}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-      </table>
-    </div>
+    <TableFixedHeader
+      headers={[
+        'Ind. tark. vuokar (€)',
+        'Käyttötarkoitus',
+        'Voimassaoloaika',
+        'Laskentakerroin',
+      ]}
+      body={getTableBody(indexAdjustedRents)}
+    />
   );
 };
 
