4a7431dbf4511cf78dcf09a1ee3866642da57e1c
Jori Lindell <jori.lindell@gmail.com>
2018-02-01 12:46:03 +0200

    Added link to the Search side menu link

--- src/components/sideMenu/SideMenu.js
+++ src/components/sideMenu/SideMenu.js
@@ -1,21 +1,23 @@
 // @flow
 import React, {Component} from 'react';
+import {Link} from 'react-router';
 import classnames from 'classnames';
 
 type Props = {
   isOpen: boolean,
+  onLinkClick: Function,
 }
 
 class SideMenu extends Component {
   props: Props;
 
   render() {
-    const {isOpen} = this.props;
+    const {isOpen, onLinkClick} = this.props;
 
     return (
       <div className={classnames('side-menu', {'is-menu-open': isOpen})}>
         <ul>
-          <li>Haku</li>
+          <li onClick={onLinkClick}><Link to="/leases">Haku</Link></li>
           <li>Raportointi</li>
           <li>Tietoa palvelusta</li>
         </ul>
