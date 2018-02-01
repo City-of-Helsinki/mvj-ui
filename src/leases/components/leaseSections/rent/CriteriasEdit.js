336354ba8c86b69e1622bb5c9429c2e11234bb94
Jori Lindell <jori.lindell@gmail.com>
2018-02-01 17:22:02 +0200

    Fixed criterias form layout on rents tab

--- src/leases/components/leaseSections/rent/CriteriasEdit.js
+++ src/leases/components/leaseSections/rent/CriteriasEdit.js
@@ -15,81 +15,119 @@ type Props = {
 const CriteriasEdit = ({fields}: Props) => {
   return (
     <div className="bordered-box">
-      {fields && fields.length > 0 && fields.map((item, index) => {
-        return (
-          <div key={index} className='item'>
-            <button
-              className='remove-button'
-              type="button"
-              title="Poista vuokranperuste"
-              onClick={() => fields.remove(index)}>
-              <img src={trashIcon} alt='Poista' />
-            </button>
+      {fields && fields.length > 0 &&
+        <Row>
+          <Column small={11}>
             <Row>
               <Column medium={5}>
                 <Row>
                   <Column small={4}>
-                    <Field
-                      component={FieldTypeSelect}
-                      label="Käyttötarkoitus"
-                      name={`${item}.purpose`}
-                      options={rentCriteriaPurposeOptions}
-                    />
+                    <label className="mvj-form-field-label">Käyttötarkoitus</label>
                   </Column>
                   <Column small={2}>
-                    <Field
-                      component={FieldTypeText}
-                      label="K-m2"
-                      name={`${item}.km2`}
-                    />
+                    <label className="mvj-form-field-label">K-m2</label>
                   </Column>
                   <Column small={3}>
-                    <Field
-                      component={FieldTypeText}
-                      label="Indeksi"
-                      name={`${item}.index`}
-                    />
+                    <label className="mvj-form-field-label">Indeksi</label>
                   </Column>
                   <Column small={3}>
-                    <Field
-                      component={FieldTypeText}
-                      label="€ / k-m2 (ind 100)"
-                      name={`${item}.ekm2ind100`}
-                    />
+                    <label className="mvj-form-field-label">€ / k-m2 (ind 100)</label>
                   </Column>
                 </Row>
               </Column>
               <Column medium={7}>
                 <Row>
                   <Column small={2}>
-                    <Field
-                      component={FieldTypeText}
-                      label="€ / k-m2 (ind)"
-                      name={`${item}.ekm2ind`}
-                    />
+                    <label className="mvj-form-field-label">€ / k-m2 (ind)</label>
                   </Column>
                   <Column small={2}>
-                    <Field
-                      component={FieldTypeText}
-                      label="Prosenttia"
-                      name={`${item}.percentage`}
-                    />
+                    <label className="mvj-form-field-label">Prosenttia</label>
                   </Column>
                   <Column small={4}>
-                    <Field
-                      component={FieldTypeText}
-                      label="Perusvuosivuokra €/v (ind 100)"
-                      name={`${item}.basic_rent`}
-                    />
+                    <label className="mvj-form-field-label">Perusvuosivuokra €/v (ind 100)</label>
                   </Column>
                   <Column small={4}>
-                    <Field
-                      component={FieldTypeText}
-                      label="Perusvuosivuokra €/v (ind)"
-                      name={`${item}.start_rent`}
-                    />
+                    <label className="mvj-form-field-label">Perusvuosivuokra €/v (ind)</label>
                   </Column>
                 </Row>
+              </Column>
+            </Row>
+          </Column>
+        </Row>
+      }
+      {fields && fields.length > 0 && fields.map((item, index) => {
+        return (
+          <div key={index}>
+            <Row>
+              <Column small={11}>
+                <Row>
+                  <Column medium={5}>
+                    <Row>
+                      <Column small={4}>
+                        <Field
+                          component={FieldTypeSelect}
+                          name={`${item}.purpose`}
+                          options={rentCriteriaPurposeOptions}
+                        />
+                      </Column>
+                      <Column small={2}>
+                        <Field
+                          component={FieldTypeText}
+                          name={`${item}.km2`}
+                        />
+                      </Column>
+                      <Column small={3}>
+                        <Field
+                          component={FieldTypeText}
+                          name={`${item}.index`}
+                        />
+                      </Column>
+                      <Column small={3}>
+                        <Field
+                          component={FieldTypeText}
+                          name={`${item}.ekm2ind100`}
+                        />
+                      </Column>
+                    </Row>
+                  </Column>
+                  <Column medium={7}>
+                    <Row>
+                      <Column small={2}>
+                        <Field
+                          component={FieldTypeText}
+                          name={`${item}.ekm2ind`}
+                        />
+                      </Column>
+                      <Column small={2}>
+                        <Field
+                          component={FieldTypeText}
+                          name={`${item}.percentage`}
+                        />
+                      </Column>
+                      <Column small={4}>
+                        <Field
+                          component={FieldTypeText}
+                          name={`${item}.basic_rent`}
+                        />
+                      </Column>
+                      <Column small={4}>
+                        <Field
+                          component={FieldTypeText}
+                          name={`${item}.start_rent`}
+                        />
+                      </Column>
+                    </Row>
+                  </Column>
+                </Row>
+              </Column>
+              <Column small={1}>
+                <button
+                  className='remove-button'
+                  type="button"
+                  title="Poista vuokranperuste"
+                  onClick={() => fields.remove(index)}>
+                  <img src={trashIcon} alt='Poista' />
+                </button>
               </Column>
             </Row>
           </div>
