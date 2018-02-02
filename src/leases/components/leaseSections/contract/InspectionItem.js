65dc5ac0fbaf151b529a05f0c9dce43e0e194cc6
Jori Lindell <jori.lindell@gmail.com>
2018-02-02 17:00:37 +0200

    Refactored decision tab non-edit mode

---
+++ src/leases/components/leaseSections/contract/InspectionItem.js
@@ -0,0 +1,49 @@
+// @flow
+import React from 'react';
+import {Row, Column} from 'react-foundation';
+import classNames from 'classnames';
+
+import {formatDate} from '../../../../util/helpers';
+
+type Props = {
+  inspection: Object,
+}
+
+const InspectionItem = ({inspection}: Props) => {
+  return (
+    <div>
+      <Row>
+        <Column medium={4}>
+          <label>Tarkastaja</label>
+          <p>{inspection.inspector ? inspection.inspector : '–'}</p>
+        </Column>
+        <Column medium={4}>
+          <label>Valvonta päivämäärä</label>
+          {inspection.supervision_date
+            ? (<p className={classNames({'alert': inspection.supervision_date && !inspection.supervised_date})}>
+                <i/>
+                {formatDate(inspection.supervision_date)}
+              </p>)
+            : <p>–</p>}
+        </Column>
+        <Column medium={4}>
+          <label>Valvottu päivämäärä</label>
+          {inspection.supervised_date
+            ? (<p className={classNames({'success': inspection.supervised_date})}>
+                <i/>
+                {formatDate(inspection.supervised_date)}
+              </p>)
+            : <p>–</p>}
+        </Column>
+      </Row>
+      <Row>
+        <Column medium={12}>
+          <label>Selite</label>
+          <p>{inspection.inspection_description ? inspection.inspection_description : '–'}</p>
+        </Column>
+      </Row>
+    </div>
+  );
+};
+
+export default InspectionItem;
