2b6edd257a31bbb970f4f102333c81e6d710c6b3
Jori Lindell <jori.lindell@gmail.com>
2018-02-12 11:30:16 +0200

    Created a component for abnormal debts on billing non edit view

---
+++ src/leases/components/leaseSections/billing/AbnormalDebts.js
@@ -0,0 +1,46 @@
+// @flow
+import React from 'react';
+import get from 'lodash/get';
+
+import {formatDate,
+  formatDateRange,
+  formatDecimalNumbers,
+  formatNumberWithThousandSeparator} from '../../../../util/helpers';
+
+type Props = {
+  abnormalDebts: Array<Object>
+}
+
+const AbnormalDebts = ({abnormalDebts}: Props) => {
+  return (
+    <table className="abnormal-debts-table">
+      <thead>
+        <tr>
+          <th>Vuokraaja</th>
+          <th>Hallintaosuus</th>
+          <th>Eräpäivä</th>
+          <th>Määrä</th>
+          <th>Aikaväli</th>
+        </tr>
+      </thead>
+      <tbody>
+        {abnormalDebts && abnormalDebts
+          ? (abnormalDebts.map((debt, index) => {
+            return (
+              <tr key={index}>
+                <td>{`${get(debt, 'tenant.lastname')} ${get(debt, 'tenant.firstname')}`}</td>
+                <td>{get(debt, 'tenant.bill_share') ? `${get(debt, 'tenant.bill_share')} %` : '-'}</td>
+                <td>{debt.due_date ? formatDate(debt.due_date) : '-'}</td>
+                <td>{debt.amount ? `${formatNumberWithThousandSeparator(formatDecimalNumbers(debt.amount))} €` : '-'}</td>
+                <td>{formatDateRange(debt.start_date, debt.end_date)}</td>
+              </tr>
+            );
+          }))
+          : (<tr className="no-data"><td colSpan={5}>Ei poikkeavia perintöjä</td></tr>)
+        }
+      </tbody>
+    </table>
+  );
+};
+
+export default AbnormalDebts;
