4d1e29d02489e32b850bfad60093398d91cc9bc3
Jori Lindell <jori.lindell@gmail.com>
2018-02-12 11:32:37 +0200

    Changed Billing class to const to optimise performance

--- src/leases/components/leaseSections/billing/Billing.js
+++ src/leases/components/leaseSections/billing/Billing.js
@@ -1,5 +1,5 @@
 // @flow
-import React, {Component} from 'react';
+import React from 'react';
 import {Row, Column} from 'react-foundation';
 import get from 'lodash/get';
 
@@ -10,56 +10,50 @@ type Props = {
   billing: Object,
 }
 
-class Billing extends Component {
-  props: Props
-
-  render() {
-    const {billing} = this.props;
-
-    return (
-      <div className="lease-section billing-section">
-        <Row>
-          <Column medium={9}><h1>Laskutus</h1></Column>
-          <Column medium={3}>
-            {billing.billing_started
-              ? (<p className="success">Laskutus käynnissä<i /></p>)
-              : (<p className="alert">Laskutus ei käynnissä<i /></p>)
-            }
-          </Column>
-        </Row>
-        <Row><Column><div className="separator-line"></div></Column></Row>
-        <Row><Column><h2>Laskut</h2></Column></Row>
-        <Row>
-          <Column>
-            <BillsTable
-              headers={[
-                'Vuokraaja',
-                'Osuus',
-                'Eräpäivä',
-                'Laskun numero',
-                'Laskutuskausi',
-                'Saamislaji',
-                'Laskun tila',
-                'Laskutettu',
-                'Maksamatta',
-                'Tiedote',
-                'Läh. SAP:iin',
-              ]}
-              bills={get(billing, 'bills')}
-            />
-          </Column>
-        </Row>
-        <Row><Column><h2>Poikkeavat perinnät</h2></Column></Row>
-        <Row>
-          <Column>
-            <AbnormalDebts
-              abnormalDebts={get(billing, 'abnormal_debts', [])}
-            />
-          </Column>
-        </Row>
-      </div>
-    );
-  }
-}
+const Billing = ({billing}: Props) => {
+  return (
+    <div className="lease-section billing-section">
+      <Row>
+        <Column medium={9}><h1>Laskutus</h1></Column>
+        <Column medium={3}>
+          {billing.billing_started
+            ? (<p className="success">Laskutus käynnissä<i /></p>)
+            : (<p className="alert">Laskutus ei käynnissä<i /></p>)
+          }
+        </Column>
+      </Row>
+      <Row><Column><div className="separator-line"></div></Column></Row>
+      <Row><Column><h2>Laskut</h2></Column></Row>
+      <Row>
+        <Column>
+          <BillsTable
+            headers={[
+              'Vuokraaja',
+              'Osuus',
+              'Eräpäivä',
+              'Laskun numero',
+              'Laskutuskausi',
+              'Saamislaji',
+              'Laskun tila',
+              'Laskutettu',
+              'Maksamatta',
+              'Tiedote',
+              'Läh. SAP:iin',
+            ]}
+            bills={get(billing, 'bills')}
+          />
+        </Column>
+      </Row>
+      <Row><Column><h2>Poikkeavat perinnät</h2></Column></Row>
+      <Row>
+        <Column>
+          <AbnormalDebts
+            abnormalDebts={get(billing, 'abnormal_debts', [])}
+          />
+        </Column>
+      </Row>
+    </div>
+  );
+};
 
 export default Billing;
