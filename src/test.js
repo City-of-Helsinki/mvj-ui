fe04ef27d02af4d7b17e2d4f2fe7a4e1315c66bb
Jori Lindell <jori.lindell@gmail.com>
2018-05-08 10:43:13 +0300

    Updated more libraries

--- src/test.js
+++ src/test.js
@@ -1,8 +1,8 @@
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
-import chaiJsx from 'chai-jsx';
+// import chaiJsx from 'chai-jsx';
 import sinonChai from 'sinon-chai';
 
 chai.use(chaiEnzyme());
-chai.use(chaiJsx);
+// chai.use(chaiJsx);
 chai.use(sinonChai);
