eccf09d0fd49477c4d73bf0a7ce2765d5aa2453d
Jori Lindell <jori.lindell@gmail.com>
2018-05-08 17:44:42 +0300

    Added tests for decisions

---
+++ src/decision/spec.js
@@ -0,0 +1,42 @@
+import {expect} from 'chai';
+import {
+  receiveDecisionsByLease,
+  notFound,
+} from './actions';
+import commentReducer from './reducer';
+
+describe('Decisions', () => {
+
+  describe('Reducer', () => {
+
+    describe('decisionReducer', () => {
+      it('should update decisions received by lease', () => {
+        const dummyLease = 1;
+        const dummyDecisions = [
+          {
+            id: 1,
+            label: 'Foo',
+          },
+        ];
+
+        const newState = {
+          byLease: {1: dummyDecisions},
+          isFetching: false,
+        };
+
+        const state = commentReducer({}, receiveDecisionsByLease({leaseId: dummyLease, decisions: dummyDecisions}));
+        expect(state).to.deep.equal(newState);
+      });
+
+      it('should update isFetching flag to false by notFound', () => {
+        const newState = {
+          byLease: {},
+          isFetching: false,
+        };
+
+        const state = commentReducer({}, notFound());
+        expect(state).to.deep.equal(newState);
+      });
+    });
+  });
+});
