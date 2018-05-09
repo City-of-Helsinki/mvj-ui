d94ad445880a42dd34820739a13f4f11d6d53132
Jori Lindell <jori.lindell@gmail.com>
2018-05-09 11:20:54 +0300

    Added tests fro auth

---
+++ src/auth/spec.js
@@ -0,0 +1,46 @@
+import {expect} from 'chai';
+import {
+  fetchApiToken,
+  receiveApiToken,
+  tokenNotFound,
+} from './actions';
+import authReducer from './reducer';
+
+describe('Auth', () => {
+
+  describe('Reducer', () => {
+
+    describe('invoiceReducer', () => {
+      it('should update isFetching flag to false by fetcing', () => {
+        const newState = {
+          apiToken: {},
+          isFetching: true,
+        };
+
+        const state = authReducer({}, fetchApiToken());
+        expect(state).to.deep.equal(newState);
+      });
+
+      it('should update isFetching flag to false by notFound', () => {
+        const newState = {
+          apiToken: {},
+          isFetching: false,
+        };
+
+        const state = authReducer({}, tokenNotFound());
+        expect(state).to.deep.equal(newState);
+      });
+
+      it('should update apiToken', () => {
+        const dummyApiToken = {'foo': 'Lorem ipsum'};
+        const newState = {
+          apiToken: dummyApiToken,
+          isFetching: false,
+        };
+
+        const state = authReducer({}, receiveApiToken(dummyApiToken));
+        expect(state).to.deep.equal(newState);
+      });
+    });
+  });
+});
