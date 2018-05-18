f6afe54ce1cefcbabae94671cc0de4b7da074716
Jori Lindell <jori.lindell@gmail.com>
2018-05-18 18:04:44 +0300

    Use https//dev.hel.fi/geocoder/v1/address temporary as geocoding service

--- src/components/map/GeoSearch.js
+++ src/components/map/GeoSearch.js
@@ -1,22 +1,22 @@
-// @flow
+// @ flow
 import {MapControl} from 'react-leaflet';
-import {GeoSearchControl, GoogleProvider} from 'leaflet-geosearch';
+import {GeoSearchControl} from 'leaflet-geosearch';
+import HelsinkiProvider from './HelsinkiProvider';
 
 class GeoSearch extends MapControl {
 
   createLeafletElement() {
     return GeoSearchControl({
       position: 'topright',
-      style: 'button',
+      style: 'bar',
       showMarker: false,
-      provider: new GoogleProvider(),
-
+      provider: new HelsinkiProvider(),
       showPopup: false,
       autoClose: true,
       retainZoomLevel: false,
       animateZoom: true,
       keepResult: false,
-      searchLabel: 'Hae',
+      searchLabel: 'Hae osoitteella',
     });
   }
 }
