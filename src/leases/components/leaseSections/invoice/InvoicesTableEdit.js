a707e7764cde7b16785da06abf37fcd4db80c511
Jori Lindell <jori.lindell@gmail.com>
2018-06-11 11:07:07 +0300

    Added default sorting for invoice tables

--- src/leases/components/leaseSections/invoice/InvoicesTableEdit.js
+++ src/leases/components/leaseSections/invoice/InvoicesTableEdit.js
@@ -179,7 +179,7 @@ class InvoicesTableEdit extends Component<Props, State> {
     if(showAllColumns) {
       return [
         {key: 'recipient', label: 'Vuokraaja', renderer: (val) => getContactFullName(val) || '-', ascSortFunction: (a, b) => sortStringByKeyAsc(getContactFullName(a), getContactFullName(b)), descSortFunction: (a, b) => sortStringByKeyDesc(getContactFullName(a), getContactFullName(b))},
-        {key: 'due_date', label: 'Eräpäivä', renderer: (val) => formatDate(val) || '-'},
+        {key: 'due_date', label: 'Eräpäivä', renderer: (val) => formatDate(val) || '-', defaultSorting: 'desc'},
         {key: 'id', label: 'Laskun numero', ascSortFunction: sortNumberByKeyAsc, descSortFunction: sortNumberByKeyDesc},
         {key: 'share', label: 'Osuus', renderer: (val, invoice) => getInvoiceSharePercentage(invoice) || '-'},
         {key: 'billing_period_start_date', label: 'Laskutuskausi', renderer: (val, invoice) => formatDateRange(invoice.billing_period_start_date, invoice.billing_period_end_date) || '-'},
@@ -193,7 +193,7 @@ class InvoicesTableEdit extends Component<Props, State> {
     } else {
       return [
         {key: 'recipient', label: 'Vuokraaja', renderer: (val) => getContactFullName(val) || '-', ascSortFunction: (a, b) => sortStringByKeyAsc(getContactFullName(a), getContactFullName(b)), descSortFunction: (a, b) => sortStringByKeyDesc(getContactFullName(a), getContactFullName(b))},
-        {key: 'due_date', label: 'Eräpäivä', renderer: (val) => formatDate(val) || '-'},
+        {key: 'due_date', label: 'Eräpäivä', renderer: (val) => formatDate(val) || '-', defaultSorting: 'desc'},
         {key: 'id', label: 'Laskun numero', ascSortFunction: sortNumberByKeyAsc, descSortFunction: sortNumberByKeyDesc},
         {key: 'share', label: 'Osuus', renderer: (val, invoice) => getInvoiceSharePercentage(invoice) || '-'},
       ];
