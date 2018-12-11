5e1229473294c6c42f133ddde225d13a65ad7864
Jori Lindell <jori.lindell@gmail.com>
2018-12-11 15:57:01 +0200

    Replaced deprecated window.devToolsExtension with window.__REDUX_DEVTOOLS_EXTENSION__

--- src/root/configureStore.js
+++ src/root/configureStore.js
@@ -17,7 +17,7 @@ export default () => {
 
   const enhancer = compose(
     applyMiddleware(sagaMiddleware, routerMiddleware),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   );
 
   const store = createStore(rootReducer, enhancer);
