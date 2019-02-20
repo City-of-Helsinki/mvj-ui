ab3969250812ec633d61997e6196857b6c7de1aa
Jori Lindell <jori.lindell@gmail.com>
2019-02-20 14:00:22 +0200

    Added missing usersPermissions types

--- src/usersPermissions/types.js
+++ src/usersPermissions/types.js
@@ -2,13 +2,17 @@
 
 import type {Action} from '../types';
 
+export type UserGroups = Array<string>;
+
 export type UsersPermissions = Array<Object>;
 
 export type UsersPermissionsState = {
+  groups: UserGroups,
   isFetching: boolean,
   permissions: UsersPermissions,
 };
 
 export type FetchUsersPermissionsAction = Action<'mvj/usersPermissions/FETCH_ALL', void>;
+export type ReceiveUserGroupsAction = Action<'mvj/usersPermissions/RECEIVE_GROUPS', UserGroups>;
 export type ReceiveUsersPermissionsAction = Action<'mvj/usersPermissions/RECEIVE_ALL', UsersPermissions>;
 export type NotFoundAction = Action<'mvj/usersPermissions/NOT_FOUND', void>;
