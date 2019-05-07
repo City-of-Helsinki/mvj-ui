eaa7472dfbee817c8c4cc851f84da0a05d8d0a4c
Jori Lindell <jori.lindell@gmail.com>
2019-05-07 11:16:35 +0300

    Replace authorize with authorize/ to oidc-client user manager

--- src/auth/util/user-manager.js
+++ src/auth/util/user-manager.js
@@ -1,6 +1,5 @@
 // @flow
-import {createUserManager} from 'redux-oidc';
-import {Global, WebStorageStateStore} from 'oidc-client';
+import {Global, Log, UserManager, WebStorageStateStore} from 'oidc-client';
 
 const userManagerConfig = {
   authority: process.env.OPENID_CONNECT_AUTHORITY_URL || 'https://api.hel.fi/sso/openid/',
@@ -15,7 +14,34 @@ const userManagerConfig = {
   userStore: new WebStorageStateStore({store: Global.localStorage}),
 };
 
-// $FlowFixMe
-const userManager = createUserManager(userManagerConfig);
+class MvjUserManager extends UserManager {
+  _signinStart(args, navigator, navigatorParams = {}) {
+    return navigator.prepare(navigatorParams).then(handle => {
+      Log.debug('UserManager._signinStart: got navigator window handle');
+
+      return this.createSigninRequest(args).then(signinRequest => {
+        Log.debug('UserManager._signinStart: got signin request');
+
+        if(!signinRequest.url.match('authorize/')) {
+          // Add missing / if needed
+          navigatorParams.url = signinRequest.url.replace('authorize', 'authorize/');
+        } else {
+          navigatorParams.url = signinRequest.url;
+        }
+        navigatorParams.id = signinRequest.state.id;
+
+        return handle.navigate(navigatorParams);
+      }).catch(err => {
+        if (handle.close) {
+          Log.debug('UserManager._signinStart: Error after preparing navigator, closing navigator window');
+          handle.close();
+        }
+        throw err;
+      });
+    });
+  }
+
+}
+const userManager = new MvjUserManager(userManagerConfig);
 
 export default userManager;
