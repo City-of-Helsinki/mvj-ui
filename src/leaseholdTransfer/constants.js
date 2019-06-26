1e8cbe7b7754b89de286abef9f6234a1cb9961cc
Jori Lindell <jori.lindell@gmail.com>
2019-06-26 16:40:25 +0300

    Add comments to leasehold transfer helper functions

--- src/leaseholdTransfer/constants.js
+++ src/leaseholdTransfer/constants.js
@@ -2,7 +2,15 @@
 import {TableSortOrder} from '$src/enums';
 
 /**
- * Contact list page default values
+ * Default sort key of leasehold transfer list page
+ * @readonly
+ * @const {string}
  */
 export const DEFAULT_SORT_KEY = 'decision_date';
+
+/**
+ * Default sort order of leasehold transfer list page
+ * @readonly
+ * @const {string}
+ */
 export const DEFAULT_SORT_ORDER = TableSortOrder.DESCENDING;
