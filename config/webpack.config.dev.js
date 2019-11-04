c66452c4a2b091a79268ac46e9afbab3439da6a9
Juha Kovero <juhakove@gmail.com>
2019-11-04 13:34:53 +0200

    Added favion to webpack dev config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -5,6 +5,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
@@ -137,6 +138,11 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, '../assets/images/favicon.png'),
+      persistentCache: true,
+    }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
