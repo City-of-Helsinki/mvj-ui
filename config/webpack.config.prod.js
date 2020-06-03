428944f4f8d341118d34dbbcff16f62b436de851
Juha Kovero <juhakove@gmail.com>
2020-06-03 17:22:43 +0300

    removed importWorkboxFrom

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -178,7 +178,6 @@ module.exports = {
     new WorkboxWebpackPlugin.GenerateSW({
       clientsClaim: true,
       exclude: [/\.map$/, /asset-manifest\.json$/],
-      importWorkboxFrom: 'cdn',
       navigateFallback: publicUrl + '/index.html',
       navigateFallbackDenylist: [
         // Exclude URLs starting with /_, as they're likely an API call
