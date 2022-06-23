0d7e05af7b9f17ea7ed996a9a9aa04391bd68e7c
Juha Kujala <juha.kujala@anders.fi>
2022-06-23 12:56:45 +0300

    Add service units to redux store

---
+++ src/serviceUnits/selectors.js
@@ -0,0 +1,10 @@
+// @flow
+import type {Selector} from '$src/types';
+import type {RootState} from '$src/root/types';
+import type {ServiceUnits} from './types';
+
+export const getIsFetching: Selector<boolean, void> = (state: RootState): boolean =>
+  state.serviceUnits.isFetching;
+
+export const getServiceUnits: Selector<ServiceUnits, void> = (state: RootState): ServiceUnits =>
+  state.serviceUnits.serviceUnits;
