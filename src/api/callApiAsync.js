e69380cfaa7a84a8d0059c9f3a826a4de5d2b5ad
Emilia Mäkelä <emilia.makela@vincit.fi>
2022-09-12 16:53:21 +0300

    Set Finnish as the default language for requests
    This interface is only available in Finnish anyway, so this will
    make it more consistent whenever any entries have been translated.

--- src/api/callApiAsync.js
+++ src/api/callApiAsync.js
@@ -1,6 +1,7 @@
 // @flow
 import {store} from '$src/root/startApp';
 import {getApiToken} from '$src/auth/selectors';
+import {UI_ACCEPT_LANGUAGE_VALUE} from "./constants";
 
 const callApiAsync = async(request: Request) => {
   const apiToken = await getApiToken(store.getState());
@@ -8,6 +9,11 @@ const callApiAsync = async(request: Request) => {
   if (apiToken) {
     request.headers.set('Authorization', `Bearer ${apiToken}`);
   }
+
+  request.headers.set(
+    'Accept-Language',
+    UI_ACCEPT_LANGUAGE_VALUE
+  );
 
   if (request.method === 'PATCH' || request.method === 'POST' || request.method === 'PUT') {
     request.headers.set('Content-Type', 'application/json');
--- src/api/callUploadRequest.js
+++ src/api/callUploadRequest.js
@@ -4,12 +4,18 @@ import {call, put, select} from 'redux-saga/effects';
 import {receiveError} from './actions';
 
 import {getApiToken} from '$src/auth/selectors';
+import {UI_ACCEPT_LANGUAGE_VALUE} from "./constants";
 
 function* callUploadRequest(request: Request): Generator<any, any, any> {
   const apiToken = yield select(getApiToken);
   if (apiToken) {
     request.headers.set('Authorization', `Bearer ${apiToken}`);
   }
+
+  request.headers.set(
+    'Accept-Language',
+    UI_ACCEPT_LANGUAGE_VALUE
+  );
 
   const response = yield call(fetch, request);
   const status = response.status;
