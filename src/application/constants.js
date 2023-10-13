c4e7bb42ad91910cffe76777a8061bc22f4639e9
Emilia Mäkelä <emilia.makela@vincit.fi>
2023-10-13 15:52:46 +0300

    Add validations to select special form fields
    The fields that will be validated with special validators are:
    - Personal identifier (henkilotunnus)
    - Company identifier (y-tunnus)
    - Email (sahkoposti)
    - Control shares (hallintaosuus), evaluated across the entire form
      for all applicants
    
    Any field whose identifier matches the one specified above will automatically
    get the relevant validator applied to it.

--- src/application/constants.js
+++ src/application/constants.js
@@ -4,6 +4,9 @@ import {ApplicantTypes} from '$src/application/enums';
 export const APPLICANT_SECTION_IDENTIFIER = 'hakijan-tiedot';
 export const TARGET_SECTION_IDENTIFIER = 'haettava-kohde';
 export const APPLICANT_TYPE_FIELD_IDENTIFIER = 'hakija';
+export const CONTROL_SHARE_FIELD_IDENTIFIER = 'hallintaosuus';
+export const EMAIL_FIELD_IDENTIFIER = 'sahkoposti';
+
 export const APPLICANT_MAIN_IDENTIFIERS: {
   [type: string]: {
     DATA_SECTION: string,
