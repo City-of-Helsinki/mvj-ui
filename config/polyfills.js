a9f3cafe8a0c3c93bbc976182cf4dc842d84f614
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2024-03-05 16:11:42 +0200

    url polyfill for webpack 5

--- config/polyfills.js
+++ config/polyfills.js
@@ -8,6 +8,9 @@ if (typeof Promise === 'undefined') {
   window.Promise = require('promise/lib/es6-extensions.js');
 }
 
+// for webpack 5
+require('url')
+
 // fetch() polyfill for making API calls.
 require('whatwg-fetch');
 
