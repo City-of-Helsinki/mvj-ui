8c9ebb090e2c3151163b56c1d7d56e376a488963
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2024-06-26 14:37:03 +0300

    wip 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import babel from "vite-plugin-babel";
+import babelPluginModuleResolver from "babel-plugin-module-resolver";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   
-  root: "./",
-  base: "./src",
+  base: "/",
   plugins: [
-    react({ include: /\.(js|jsx|ts|tsx)$/, jsxRuntime: "automatic" }),
+    react(),
+    // react({ include: /\.(js|jsx|ts|tsx)$/, jsxRuntime: "automatic" }),
     viteTsconfigPaths()
+    // babel()
   ],
   server: {
     // this ensures that the browser opens upon server start
