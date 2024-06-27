53fe3d96f55702a5d1e364333e300a4674267c2e
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2024-06-27 15:16:19 +0300

    invoice edit: fetch receivable types by lease's service unit

--- src/leaseCreateCharge/requests.ts
+++ src/leaseCreateCharge/requests.ts
@@ -1,7 +1,7 @@
 import callApi from "../api/callApi";
 import createUrl from "../api/createUrl";
 import { store } from "../root/startApp";
-import { getUserActiveServiceUnit } from "../usersPermissions/selectors";
+import { getCurrentLease } from '../leases/selectors';
 export const fetchAttributes = (): Generator<any, any, any> => {
   return callApi(new Request(createUrl(`lease_create_charge/`), {
     method: 'OPTIONS'
@@ -9,7 +9,8 @@ export const fetchAttributes = (): Generator<any, any, any> => {
 };
 export const fetchReceivableTypes = (): Generator<any, any, any> => {
   const state = store.getState();
-  const serviceUnit = getUserActiveServiceUnit(state);
+  const lease = getCurrentLease(state);
+  const serviceUnit = lease.service_unit;
   return callApi(new Request(createUrl(`receivable_type/`, {
     service_unit: serviceUnit.id
   }), {
