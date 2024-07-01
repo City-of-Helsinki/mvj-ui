0fa7f7df5e2849af7456a05ba6c9071875f145bf
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2024-07-01 14:30:11 +0300

    FieldTypeBoolean: cast boolean value to string

--- src/components/form/FieldTypeBoolean.tsx
+++ src/components/form/FieldTypeBoolean.tsx
@@ -51,8 +51,7 @@ const FieldTypeBoolean = ({
         label: optionLabel
       } = option;
       return <label key={index} className='option-label'>
-            {/** @ts-ignore: Type 'boolean' is not assignable to type 'string | number | readonly string[]'. */}
-            <input type='radio' checked={optionValue === value} name={name} onChange={handleChange} value={optionValue} />
+            <input type='radio' checked={optionValue === value} name={name} onChange={handleChange} value={Boolean(optionValue).toString()} />
             <span>{optionLabel}</span>
           </label>;
     })}
