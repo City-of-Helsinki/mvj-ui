b12377415cb5ab8f9964f9933eeb24abac7f16cd
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-08-13 16:01:58 +0300

    add vitest, remove mocha with old test setup

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,25 @@ import react from '@vitejs/plugin-react';
 import commonjs from 'vite-plugin-commonjs';
 import path from 'path';
 import postcssUrl from 'postcss-url';
+import babel from '@rollup/plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'classic'
+      jsxRuntime: 'classic',
     }),
     commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.jsx'],
+      include: ['node_modules/react-foundation/**'],
+      only: ['node_modules/react-foundation/**'],
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+      ],
+    }),
   ],
   css: {
     postcss: {
@@ -42,5 +53,15 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+    },
+    coverage: {
+      provider: 'istanbul'
+    },
   }
 })
