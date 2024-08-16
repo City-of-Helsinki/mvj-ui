9c85cbc2019063f0308dccfe3e9fe62902817d10
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-08-16 13:14:46 +0300

    update types, restrict react types to ^16

--- types/proj4leaflet.d.ts
+++
@@ -1,9 +0,0 @@
-import * as L from 'leaflet';
-
-declare module 'leaflet' {
-    namespace Proj {
-        class CRS extends L.CRS {
-            constructor(code: string, proj4def: string, options?: any);
-          }
-    }
-}
