7772e65265593f85482d59a0fd7fe7b474493c9e
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-09-13 09:35:16 +0300

    move user in redux store under auth, fix some tests

--- src/auth/reducer.ts
+++ src/auth/reducer.ts
@@ -4,26 +4,20 @@ import type { Reducer } from "@/types";
 import type { ReceiveApiTokenAction, ReceiveUserAction } from "./types";
 
 const apiTokenReducer: Reducer<any> = handleActions({
-  ['mvj/auth/CLEAR_API_TOKEN']: () => {},
+  ['mvj/auth/CLEAR_API_TOKEN']: () => null,
   ['mvj/auth/RECEIVE_API_TOKEN']: (state: any, {
     payload
   }: ReceiveApiTokenAction) => payload
-}, {});
+}, null);
+
+const userReducer: Reducer<any> = handleActions({
+    ['mvj/auth/USER_FOUND']: (state: any, {
+      payload 
+    }: ReceiveUserAction) => payload,
+    ['mvj/auth/CLEAR_USER']: () => null,
+}, null);
 
 export const authReducer = combineReducers<Record<string, any>, any>({
   apiToken: apiTokenReducer,
-});
-
-const userReducer: Reducer<any> = handleActions(
-  {
-    'mvj/auth/USER_FOUND': (state: any, {
-      payload 
-    }: ReceiveUserAction) => payload,
-    'mvj/auth/CLEAR_USER': () => null,
-  },
-  null
-);
-
-export const oidcReducer = combineReducers({
   user: userReducer,
 });
