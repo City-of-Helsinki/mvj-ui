40708551f6fa918f06801d88c965f99ee8b5d504
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-09-13 08:11:17 +0300

    add custom auth hook, add tests

--- src/leaseCreateCharge/requests.ts
+++ src/leaseCreateCharge/requests.ts
@@ -1,7 +1,7 @@
 import callApi from "@/api/callApi";
 import createUrl from "@/api/createUrl";
 import { store } from "@/index";
-import { getCurrentLease } from '../leases/selectors';
+import { getCurrentLease } from '@/leases/selectors';
 export const fetchAttributes = (): Generator<any, any, any> => {
   return callApi(new Request(createUrl(`lease_create_charge/`), {
     method: 'OPTIONS'
