eb0c5f3304b993f1c703c1cf922ab5425b61a6b7
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-09-17 10:37:28 +0300

    Configure Keycloak

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,6 +1,7 @@
 import type { LoginProviderProps } from 'hds-react';
 
-export const loginProviderProperties: LoginProviderProps = {
+// Tunnistamo SSO (legacy)
+const loginProviderTunnistamoProperties: LoginProviderProps = {
   userManagerSettings: {
     authority: import.meta.env.VITE_OPENID_CONNECT_AUTHORITY_URL || 'https://api.hel.fi/sso/openid/',
     client_id: import.meta.env.VITE_OPENID_CONNECT_CLIENT_ID || '',
@@ -10,3 +11,26 @@ export const loginProviderProperties: LoginProviderProps = {
   apiTokensClientSettings: { url: import.meta.env.VITE_OPENID_CONNECT_API_TOKEN_URL },
   sessionPollerSettings: { pollIntervalInMs: 300000 } // 300000ms = 5min
 };
+
+// Tunnistus SSO
+const loginProviderTunnistusProperties: LoginProviderProps = {
+  userManagerSettings: {
+    authority: import.meta.env.VITE_TUNNISTUS_OIDC_AUTHORITY_URL || 'https://tunnistus.hel.fi/auth/realms/helsinki-tunnistus',
+    client_id: import.meta.env.VITE_TUNNISTUS_OIDC_CLIENT_ID || '',
+    scope: import.meta.env.VITE_TUNNISTUS_OIDC_SCOPE || 'openid profile',
+    redirect_uri: `${location.origin}/callback`,
+  },
+  apiTokensClientSettings: {
+    url: import.meta.env.VITE_TUNNISTUS_OIDC_API_TOKEN_URL || 'https://tunnistus.hel.fi/auth/realms/helsinki-tunnistus/protocol/openid-connect/token',
+    queryProps: {
+      grantType: 'urn:ietf:params:oauth:grant-type:uma-ticket',
+      permission: '#access',
+    },
+    audiences: [import.meta.env.VITE_TUNNISTUS_OIDC_API_AUDIENCE],
+  },
+  sessionPollerSettings: { pollIntervalInMs: 300000 } // 300000ms = 5min
+};
+
+export const useTunnistamoOpenIdConnect = import.meta.env.VITE_USE_TUNNISTAMO_OPENID_CONNECT === 'true' || import.meta.env.VITE_USE_TUNNISTAMO_OPENID_CONNECT === true;
+// By default use Tunnistus SSO
+export const loginProviderProperties = useTunnistamoOpenIdConnect ? loginProviderTunnistamoProperties : loginProviderTunnistusProperties;
