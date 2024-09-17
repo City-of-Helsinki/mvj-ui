4c14f01cd065542bc1a3df5161f21becad79f5a1
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-09-17 10:37:28 +0300

    Configure Keycloak

--- src/auth/selectors.ts
+++ src/auth/selectors.ts
@@ -1,6 +1,12 @@
 import type { Selector } from "@/types";
 import type { ApiToken, AuthState } from "./types";
 import type { User } from 'hds-react';
+import { useTunnistamoOpenIdConnect } from "@/auth/constants";
 // Helper functions to select state
-export const getApiToken: Selector<ApiToken, void> = (state: Record<string, any>): AuthState => state.auth.apiToken[import.meta.env.VITE_OPENID_CONNECT_API_TOKEN_KEY || 'https://api.hel.fi/auth/mvj'];
+export const getApiToken: Selector<ApiToken, void> = (state: Record<string, any>): AuthState => {
+  if (useTunnistamoOpenIdConnect) {
+    return state.auth.apiToken[import.meta.env.VITE_OPENID_CONNECT_API_TOKEN_KEY || 'https://api.hel.fi/auth/mvj'];
+  }
+  return state.auth.apiToken[import.meta.env.VITE_TUNNISTUS_OIDC_API_AUDIENCE || 'mvj-api'];
+};
 export const getLoggedInUser: Selector<Record<string, any>, void> = (state: Record<string, any>): User | null => state.auth.user;
