5bbca7fe2489de9a3f8c76a9c3ea1ce5f11e2dc2
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-09-18 16:19:21 +0300

    auth reducer use empty string as default

--- src/auth/reducer.ts
+++ src/auth/reducer.ts
@@ -4,18 +4,18 @@ import type { Reducer } from "@/types";
 import type { ReceiveApiTokenAction, ReceiveUserAction } from "./types";
 
 const apiTokenReducer: Reducer<any> = handleActions({
-  ['mvj/auth/CLEAR_API_TOKEN']: () => null,
+  ['mvj/auth/CLEAR_API_TOKEN']: () => '',
   ['mvj/auth/RECEIVE_API_TOKEN']: (state: any, {
     payload
   }: ReceiveApiTokenAction) => payload
-}, null);
+}, '');
 
 const userReducer: Reducer<any> = handleActions({
     ['mvj/auth/USER_FOUND']: (state: any, {
       payload 
     }: ReceiveUserAction) => payload,
-    ['mvj/auth/CLEAR_USER']: () => null,
-}, null);
+    ['mvj/auth/CLEAR_USER']: () => '',
+}, '');
 
 export const authReducer = combineReducers<Record<string, any>, any>({
   apiToken: apiTokenReducer,
