7a6710e21981bff8c8f5590f91fd71c74c849faf
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-09-25 13:28:21 +0300

    refactor sso provider selection
    avoid using boolean values in envvars as yaml, ansible, .env evaluate them differently

--- src/auth/selectors.ts
+++ src/auth/selectors.ts
@@ -1,12 +1,9 @@
 import type { Selector } from "@/types";
 import type { ApiToken, AuthState } from "./types";
 import type { User } from 'hds-react';
-import { useTunnistamoOpenIdConnect } from "@/auth/constants";
+import { apiTokenKeyName } from "@/auth/constants";
 // Helper functions to select state
 export const getApiToken: Selector<ApiToken, void> = (state: Record<string, any>): AuthState => {
-  if (useTunnistamoOpenIdConnect) {
-    return state.auth.apiToken[import.meta.env.VITE_OPENID_CONNECT_API_TOKEN_KEY || 'https://api.hel.fi/auth/mvj'];
-  }
-  return state.auth.apiToken[import.meta.env.VITE_TUNNISTUS_OIDC_API_AUDIENCE || 'mvj-api'];
+  return state.auth.apiToken[apiTokenKeyName];
 };
 export const getLoggedInUser: Selector<Record<string, any>, void> = (state: Record<string, any>): User | null => state.auth.user;
