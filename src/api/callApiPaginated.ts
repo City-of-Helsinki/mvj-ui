02a5a7c37a60c78217c3e2fd8c1308e3b894db31
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2024-10-21 12:30:28 +0300

    create api response pagination

---
+++ src/api/callApiPaginated.ts
@@ -0,0 +1,46 @@
+import callApi from "./callApi";
+
+function* callApiPaginated(request: Request): Generator<any, any, any> {
+  let { response, bodyAsJson } = yield callApi(request);
+  const allResults = [...bodyAsJson.results];
+
+  if (bodyAsJson.next) {
+    let nextUrl = bodyAsJson.next;
+    while (nextUrl !== null) {
+      request = new Request(nextUrl, {
+        method: 'GET'
+      });
+      ({ response, bodyAsJson } = yield callApi(request));
+      const status = response.status;
+
+      switch (status) {
+        case 200:
+          allResults.push(...bodyAsJson.results);
+          break;
+        case 204:
+          return {
+            response
+          };
+          break;
+      
+        default:
+          return {
+            response,
+            bodyAsJson: {
+              exception: response.status,
+              message: response.statusText
+            }
+          };
+          break;
+      }
+      
+      nextUrl = bodyAsJson.next || null;
+    }
+  }
+
+  bodyAsJson.results = allResults;
+
+  return { response, bodyAsJson };
+}
+
+export default callApiPaginated;
