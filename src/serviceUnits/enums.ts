5d5812ad38d2aa59a9c1071a04ed6c80227bfff4
Juho Kettunen <juho.kettunen@nordcloud.com>
2024-11-07 15:48:31 +0200

    Add enumeration for service unit IDs

---
+++ src/serviceUnits/enums.ts
@@ -0,0 +1,20 @@
+/**
+ * Expected IDs of the ServiceUnit model in fixtures and database.
+ *
+ * In Finnish: Palvelukokonaisuuden tunniste
+ *
+ * - MAKE = Maaomaisuuden kehittäminen ja tontit
+ * - AKV = Alueiden käyttö ja valvonta
+ * - KUVA_LIPA = Kulttuuri ja vapaa-aika / Liikuntapalvelut
+ * - KUVA_UPA = Kulttuuri ja vapaa-aika / Ulkoilupalvelut
+ * - KUVA_NUP = Kulttuuri ja vapaa-aika / Nuorisopalvelut
+ * @readonly
+ * @type {{}}
+ */
+export const ServiceUnitIds = {
+  MAKE: 1,
+  AKV: 2,
+  KUVA_LIPA: 3,
+  KUVA_UPA: 4,
+  KUVA_NUP: 5
+}
