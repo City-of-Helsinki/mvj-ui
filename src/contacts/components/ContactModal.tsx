1d2990cd634f497d690363bda4112725bfa176f0
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-05-08 13:22:13 +0300

    ContactModal to final-form
    fixes to TenantsEdit and LitigantsEdit related to their usage of ContactModal

--- src/contacts/components/ContactModal.tsx
+++ src/contacts/components/ContactModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
+import { Form } from "react-final-form";
 import Button from "@/components/button/Button";
 import ContactForm from "./forms/ContactForm";
 import GreenBox from "@/components/content/GreenBox";
@@ -7,32 +7,28 @@ import Modal from "@/components/modal/Modal";
 import { ButtonColors } from "@/components/enums";
 import { Methods } from "@/enums";
 import { isMethodAllowed } from "@/util/helpers";
-import {
-  getIsContactFormValid,
-  getIsSaveClicked,
-  getMethods as getContactMethods,
-} from "@/contacts/selectors";
-import type { Methods as MethodsType } from "types";
-import type { RootState } from "@/root/types";
+import { useContactAttributes } from "@/components/attributes/ContactAttributes";
+import type { FormApi } from "final-form";
+import { useSelector } from "react-redux";
+import { getUserActiveServiceUnit } from "@/usersPermissions/selectors";
+import { ServiceUnit } from "@/serviceUnits/types";
+import { getInitialContactFormValues } from "../selectors";
+import { Contact } from "@/contacts/types";
+
 type Props = {
-  contactMethods: MethodsType;
-  isContactFormValid: boolean;
   isOpen: boolean;
-  isSaveClicked: boolean;
   onCancel: (...args: Array<any>) => any;
   onClose: (...args: Array<any>) => any;
-  onSave: (...args: Array<any>) => any;
+  onSave: (values: Partial<Contact>, isValid: boolean) => any;
   onSaveAndAdd: (...args: Array<any>) => any;
   showSave: boolean;
   showSaveAndAdd: boolean;
   title: string;
+  serviceUnit: ServiceUnit;
 };
 
 const ContactModal = ({
-  contactMethods,
-  isContactFormValid,
   isOpen,
-  isSaveClicked,
   onCancel,
   onClose,
   onSave,
@@ -40,7 +36,18 @@ const ContactModal = ({
   showSave = true,
   showSaveAndAdd,
   title,
+  serviceUnit,
 }: Props) => {
+  const { contactMethods } = useContactAttributes();
+  const initialContactFormValues = useSelector(getInitialContactFormValues);
+
+  const userActiveServiceUnit = useSelector(getUserActiveServiceUnit);
+
+  const handleSubmit = (values: any, form: FormApi) => {
+    const isValid = form?.getState().valid;
+    onSave(values, isValid);
+  };
+
   return (
     <div className="contact-modal">
       <Modal
@@ -49,52 +56,59 @@ const ContactModal = ({
         onClose={onClose}
         title={title || "Uusi asiakas"}
       >
-        <div>
-          <GreenBox className="no-margin">
-            {isOpen && <ContactForm isFocusedOnMount />}
-          </GreenBox>
+        <Form
+          onSubmit={handleSubmit}
+          initialValues={{
+            ...initialContactFormValues,
+            // If initial values are set it is copying, otherwise creating a new contact
+            service_unit: initialContactFormValues?.service_unit?.id
+              ? initialContactFormValues.service_unit
+              : // Allows passing a service unit as a prop in cases when users active service unit could be incorrect
+                serviceUnit || userActiveServiceUnit,
+          }}
+          keepDirtyOnReinitialize={true}
+        >
+          {({ handleSubmit, form, submitting, pristine, valid }) => (
+            <div>
+              <GreenBox className="no-margin">
+                {isOpen && <ContactForm formApi={form} isFocusedOnMount />}
+              </GreenBox>
 
-          <div className="button-wrapper">
-            <Button
-              className={ButtonColors.SECONDARY}
-              onClick={onCancel}
-              text="Peruuta"
-            />
-            {showSave && (
-              <Button
-                className={ButtonColors.SUCCESS}
-                disabled={
-                  !isMethodAllowed(contactMethods, Methods.PATCH) ||
-                  (isSaveClicked && !isContactFormValid)
-                }
-                onClick={onSave}
-                text="Tallenna"
-              />
-            )}
-            {showSaveAndAdd && (
-              <Button
-                className={ButtonColors.SUCCESS}
-                disabled={
-                  !isMethodAllowed(contactMethods, Methods.POST) ||
-                  (isSaveClicked && !isContactFormValid)
-                }
-                onClick={onSaveAndAdd}
-                text="Tallenna ja Lisää"
-              />
-            )}
-          </div>
-        </div>
+              <div className="button-wrapper">
+                <Button
+                  className={ButtonColors.SECONDARY}
+                  onClick={onCancel}
+                  text="Peruuta"
+                />
+                {showSave && (
+                  <Button
+                    className={ButtonColors.SUCCESS}
+                    disabled={
+                      !isMethodAllowed(contactMethods, Methods.PATCH) ||
+                      (submitting && !valid)
+                    }
+                    onClick={handleSubmit}
+                    text="Tallenna"
+                  />
+                )}
+                {showSaveAndAdd && (
+                  <Button
+                    className={ButtonColors.SUCCESS}
+                    disabled={
+                      !isMethodAllowed(contactMethods, Methods.POST) ||
+                      (submitting && !valid)
+                    }
+                    onClick={handleSubmit}
+                    text="Tallenna ja Lisää"
+                  />
+                )}
+              </div>
+            </div>
+          )}
+        </Form>
       </Modal>
     </div>
   );
 };
 
-const mapStateToProps = (state: RootState) => {
-  return {
-    contactMethods: getContactMethods(state),
-    isContactFormValid: getIsContactFormValid(state),
-    isSaveClicked: getIsSaveClicked(state),
-  };
-};
-
-export default connect(mapStateToProps)(ContactModal);
+export default ContactModal;
