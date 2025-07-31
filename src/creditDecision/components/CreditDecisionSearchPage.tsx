5a6ef49e27335f14f1e646c364c9dfeb5a235b33
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2025-07-31 13:25:21 +0300

    credit decision to final-form

--- src/creditDecision/components/CreditDecisionSearchPage.tsx
+++ src/creditDecision/components/CreditDecisionSearchPage.tsx
@@ -1,6 +1,5 @@
-import React, { Fragment, PureComponent } from "react";
+import React, { Fragment, useEffect, useState, useCallback } from "react";
 import { connect } from "react-redux";
-import { initialize } from "redux-form";
 import { withRouter } from "react-router";
 import flowRight from "lodash/flowRight";
 import isEmpty from "lodash/isEmpty";
@@ -15,7 +14,7 @@ import CreditDecisionTemplate from "@/creditDecision/components/CreditDecisionTe
 import { ContactTypes } from "@/contacts/enums";
 import { CreditDecisionText, SanctionsCheckText } from "@/creditDecision/enums";
 import { receiveTopNavigationSettings } from "@/components/topNavigation/actions";
-import { FormNames, PermissionMissingTexts } from "@/enums";
+import { PermissionMissingTexts } from "@/enums";
 import { UsersPermissions } from "@/usersPermissions/enums";
 import {
   hasPermissions,
@@ -29,32 +28,28 @@ import {
   getUsersPermissions,
 } from "@/usersPermissions/selectors";
 import type { UsersPermissions as UsersPermissionsType } from "@/usersPermissions/types";
+
 type Props = {
   history: Record<string, any>;
-  initialize: (...args: Array<any>) => any;
   isFetchingUsersPermissions: boolean;
   location: Record<string, any>;
   receiveTopNavigationSettings: (...args: Array<any>) => any;
   usersPermissions: UsersPermissionsType;
 };
-type State = {
-  contactType: String;
-  keyword: String;
-};
 
-class CreditDecisionSearchPage extends PureComponent<Props, State> {
-  state = {
-    contactType: "",
-    keyword: "",
-  };
+const CreditDecisionSearchPage: React.FC<Props> = ({
+  history,
+  isFetchingUsersPermissions,
+  location,
+  receiveTopNavigationSettings,
+  usersPermissions,
+}) => {
+  const [contactType, setContactType] = useState<string>("");
+  const [keyword, setKeyword] = useState<string>("");
+  const [searchFormInitialValues, setSearchFormInitialValues] = useState<Record<string, any>>({});
 
-  componentDidMount() {
-    const {
-      initialize,
-      location: { search },
-      receiveTopNavigationSettings,
-    } = this.props;
-    const query = getUrlParams(search);
+  useEffect(() => {
+    const query = getUrlParams(location.search);
     setPageTitle("Asiakastieto");
     receiveTopNavigationSettings({
       linkUrl: getRouteById(Routes.CREDIT_DECISION),
@@ -62,124 +57,99 @@ class CreditDecisionSearchPage extends PureComponent<Props, State> {
       showSearch: false,
     });
 
-    if (query.contact_type) {
-      this.setState({
-        contactType: query.contact_type,
-      });
-    }
+    setContactType(query.contact_type || "");
+    setKeyword(query.keyword || "");
+    setSearchFormInitialValues({
+      contact_type: query.contact_type || "",
+      keyword: query.keyword || "",
+    });
+  }, []);
 
-    if (query.keyword) {
-      this.setState({
-        keyword: query.keyword,
-      });
-    }
+  useEffect(() => {
+    const query = getUrlParams(location.search);
+    setContactType(query.contact_type || "");
+    setKeyword(query.keyword || "");
+    setSearchFormInitialValues({
+      contact_type: query.contact_type || "",
+      keyword: query.keyword || "",
+    });
+  }, [location.search]);
 
-    initialize(FormNames.CREDIT_DECISION_SEARCH, query);
-  }
+  const handleSearchChange = useCallback((query) => {
+    setContactType(query.contact_type);
+    setKeyword(query.keyword);
+    setSearchFormInitialValues({
+      contact_type: query.contact_type,
+      keyword: query.keyword,
+    });
+    history.push({
+      pathname: getRouteById(Routes.CREDIT_DECISION),
+      search: getSearchQuery(query),
+    });
+  }, [history]);
 
-  componentDidUpdate(prevProps) {
-    const {
-      location: { search: currentSearch },
-      initialize,
-    } = this.props;
-    const {
-      location: { search: prevSearch },
-    } = prevProps;
-    const searchQuery = getUrlParams(currentSearch);
-
-    if (currentSearch !== prevSearch) {
-      this.setState(
-        {
-          contactType: searchQuery.contact_type || "",
-          keyword: searchQuery.keyword || "",
-        },
-        () => {
-          initialize(FormNames.CREDIT_DECISION_SEARCH, {
-            contact_type: searchQuery.contact_type || "",
-            keyword: searchQuery.keyword || "",
-          });
-        },
-      );
-    }
-  }
-
-  handleSearchChange = (query) => {
-    const { history } = this.props;
-    this.setState(
-      {
-        contactType: query.contact_type,
-        keyword: query.keyword,
-      },
-      history.push({
-        pathname: getRouteById(Routes.CREDIT_DECISION),
-        search: getSearchQuery(query),
-      }),
-    );
-  };
-
-  render() {
-    const { isFetchingUsersPermissions, usersPermissions } = this.props;
-    const { contactType, keyword } = this.state;
-    if (isFetchingUsersPermissions)
-      return (
-        <PageContainer>
-          <Loader isLoading={true} />
-        </PageContainer>
-      );
-    if (isEmpty(usersPermissions)) return null;
-    if (!hasPermissions(usersPermissions, UsersPermissions.VIEW_CREDITDECISION))
-      return (
-        <PageContainer>
-          <AuthorizationError text={PermissionMissingTexts.CREDIT_DECISION} />
-        </PageContainer>
-      );
+  if (isFetchingUsersPermissions)
     return (
       <PageContainer>
-        <ContentContainer>
-          <h2>{CreditDecisionText.SEARCH_TITLE}</h2>
-          <Divider />
-          <SearchForm onSearch={this.handleSearchChange} />
-
-          {contactType && (
-            <Fragment>
-              {contactType === ContactTypes.BUSINESS && (
-                <Fragment>
-                  <h2>
-                    {CreditDecisionText.BUSINESS_TITLE}: {keyword}
-                  </h2>
-                  <Divider />
-                  <CreditDecisionTemplate
-                    businessId={keyword}
-                    contactType={ContactTypes.BUSINESS}
-                  />
-                </Fragment>
-              )}
-
-              {contactType === ContactTypes.PERSON && (
-                <Fragment>
-                  <h2>
-                    {CreditDecisionText.PERSON_TITLE}: {keyword}
-                  </h2>
-                  <Divider />
-                  <CreditDecisionTemplate
-                    nin={keyword}
-                    contactType={ContactTypes.PERSON}
-                  />
-                </Fragment>
-              )}
-            </Fragment>
-          )}
-        </ContentContainer>
-        <Divider />
-        <ContentContainer>
-          <h2>{SanctionsCheckText.SEARCH_TITLE}</h2>
-          <Divider />
-          <SanctionsCheckForm />
-        </ContentContainer>
+        <Loader isLoading={true} />
       </PageContainer>
     );
-  }
-}
+  if (isEmpty(usersPermissions)) return null;
+  if (!hasPermissions(usersPermissions, UsersPermissions.VIEW_CREDITDECISION))
+    return (
+      <PageContainer>
+        <AuthorizationError text={PermissionMissingTexts.CREDIT_DECISION} />
+      </PageContainer>
+    );
+  return (
+    <PageContainer>
+      <ContentContainer>
+        <h2>{CreditDecisionText.SEARCH_TITLE}</h2>
+        <Divider />
+        <SearchForm
+          onSearch={handleSearchChange}
+          initialValues={searchFormInitialValues}
+        />
+
+        {contactType && (
+          <Fragment>
+            {contactType === ContactTypes.BUSINESS && (
+              <Fragment>
+                <h2>
+                  {CreditDecisionText.BUSINESS_TITLE}: {keyword}
+                </h2>
+                <Divider />
+                <CreditDecisionTemplate
+                  businessId={keyword}
+                  contactType={ContactTypes.BUSINESS}
+                />
+              </Fragment>
+            )}
+
+            {contactType === ContactTypes.PERSON && (
+              <Fragment>
+                <h2>
+                  {CreditDecisionText.PERSON_TITLE}: {keyword}
+                </h2>
+                <Divider />
+                <CreditDecisionTemplate
+                  nin={keyword}
+                  contactType={ContactTypes.PERSON}
+                />
+              </Fragment>
+            )}
+          </Fragment>
+        )}
+      </ContentContainer>
+      <Divider />
+      <ContentContainer>
+        <h2>{SanctionsCheckText.SEARCH_TITLE}</h2>
+        <Divider />
+        <SanctionsCheckForm />
+      </ContentContainer>
+    </PageContainer>
+  );
+};
 
 export default flowRight(
   withRouter,
@@ -191,7 +161,6 @@ export default flowRight(
       };
     },
     {
-      initialize,
       receiveTopNavigationSettings,
     },
   ),
