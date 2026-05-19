98e333c512af99590c3e79083c32d3c9eb1d651c
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-05-19 12:54:06 +0000

    add new options to invoice state

--- apps/landuse/src/landUse/components/tabs/LandUseInvoicing.tsx
+++ apps/landuse/src/landUse/components/tabs/LandUseInvoicing.tsx
@@ -218,7 +218,7 @@ const createEmptyInvoiceRow = (
   dueDate: "",
   invoiceNumber: "",
   type: undefined,
-  status: undefined,
+  status: "Luonnos",
   billedAmount: "",
   remainingAmount: "",
   invoiceRows: [],
