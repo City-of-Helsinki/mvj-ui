82f32d515577dbc3c00e46ef994c5dca78fcf31e
Kimi Ihalainen <kimi.ihalainen@nordcloud.com>
2026-06-09 12:50:36 +0300

    Convert basis of rent calculator page to redux form

--- apps/leasing/src/basisOfRentCalculator/components/BasisOfRentCalculatorForm.tsx
+++ apps/leasing/src/basisOfRentCalculator/components/BasisOfRentCalculatorForm.tsx
@@ -1,43 +1,47 @@
 import React from "react";
-import { FieldArray } from "react-final-form-arrays";
-import arrayMutators from "final-form-arrays";
+import { FieldArray, formValueSelector, reduxForm } from "redux-form";
 import BasisOfRentsEdit from "@/leases/components/leaseSections/rent/basisOfRent/BasisOfRentsEdit";
 import Divider from "@/components/content/Divider";
 import Title from "@/components/content/Title";
-import { Form } from "react-final-form";
+import { FormNames } from "@/enums";
 import {
   LeaseBasisOfRentsFieldPaths,
   LeaseBasisOfRentsFieldTitles,
 } from "@/leases/enums";
 import { getUiDataLeaseKey } from "@/uiData/helpers";
+import { useSelector } from "react-redux";
 
+const formName = FormNames.BASIS_OF_RENT_CALCULATOR;
 const RentsEdit: React.FC = () => {
+  const selector = formValueSelector(formName);
+  const basisOfRents =
+    useSelector((state) => selector(state, "basis_of_rents")) || [];
   return (
-    <Form onSubmit={() => {}} mutators={{ ...arrayMutators }}>
-      {({ handleSubmit }) => (
-        <form onSubmit={handleSubmit}>
-          <Title
-            enableUiDataEdit
-            uiDataKey={getUiDataLeaseKey(
-              LeaseBasisOfRentsFieldPaths.BASIS_OF_RENTS,
-            )}
-          >
-            {LeaseBasisOfRentsFieldTitles.BASIS_OF_RENTS}
-          </Title>
-          <Divider />
-          <FieldArray name="basis_of_rents">
-            {(fieldArrayProps) =>
-              BasisOfRentsEdit({
-                ...fieldArrayProps,
-                archived: false,
-                basisOfRents: [],
-              })
-            }
-          </FieldArray>
-        </form>
-      )}
-    </Form>
+    <form>
+      <Title
+        enableUiDataEdit
+        uiDataKey={getUiDataLeaseKey(
+          LeaseBasisOfRentsFieldPaths.BASIS_OF_RENTS,
+        )}
+      >
+        {LeaseBasisOfRentsFieldTitles.BASIS_OF_RENTS}
+      </Title>
+      <Divider />
+      <FieldArray
+        archived={false}
+        component={BasisOfRentsEdit}
+        addButtonClass="no-bottom-margin"
+        basisOfRents={basisOfRents}
+        formName={formName}
+        name="basis_of_rents"
+        showLockedAt={false}
+        showPlansInspectedAt={false}
+      />
+    </form>
   );
 };
 
-export default RentsEdit;
+export default reduxForm({
+  form: formName,
+  destroyOnUnmount: false,
+})(RentsEdit) as React.ComponentType<any>;
