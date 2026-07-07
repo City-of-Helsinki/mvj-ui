74c3c21a9db349f8603ed0a9dbf8472c1b2f5af8
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-07-07 12:03:26 +0000

    duplicate content component styles for easier hds migration

---
+++ apps/leasing/src/components/content/PageContainerHDS.tsx
@@ -0,0 +1,48 @@
+import React, { useCallback, useEffect, useRef } from "react";
+import classNames from "classnames";
+
+type Props = {
+  children?: React.ReactNode;
+  className?: string;
+  hasTabs?: boolean;
+};
+
+const PageContainerHDS: React.FC<Props> = ({
+  children,
+  className,
+  hasTabs,
+}) => {
+  const componentRef = useRef<HTMLDivElement | null>(null);
+
+  const handleResize = useCallback(() => {
+    const pageNavigation = document.getElementsByClassName(
+      "content__page-navigator-wrapper",
+    );
+
+    if (pageNavigation.length && componentRef.current) {
+      const { height } = pageNavigation[0].getClientRects()[0];
+      componentRef.current.style.marginTop = height + "px";
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [handleResize]);
+
+  return (
+    <div
+      ref={componentRef}
+      className={classNames("content-hds__page-container", className)}
+      style={{
+        paddingTop: hasTabs ? 0 : null,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default PageContainerHDS;
