2074e825036a46677c8642523b127d2c4e65f176
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-07-07 05:55:59 +0000

    MultiItemCollapse to functional & add prop to use HDS Tag for counts

--- apps/leasing/src/components/table/MultiItemCollapse.tsx
+++ apps/leasing/src/components/table/MultiItemCollapse.tsx
@@ -1,47 +1,54 @@
-import React, { PureComponent } from "react";
+import React, { memo } from "react";
 import classNames from "classnames";
 import ListItem from "@/components/content/ListItem";
+import { Tag, TagSize } from "hds-react";
 type Props = {
   items: Array<any>;
   itemRenderer: (...args: Array<any>) => any;
   open: boolean;
+  useTagForCount?: boolean;
 };
 
-class MultiItemCollapse extends PureComponent<Props> {
-  render() {
-    const { items, itemRenderer, open } = this.props;
+const MultiItemCollapse: React.FC<Props> = ({
+  items,
+  itemRenderer,
+  open,
+  useTagForCount: useHdsTagForCount,
+}) => {
+  if (!items || !items.length) {
+    return "-";
+  }
 
-    if (!items || !items.length) {
-      return "-";
-    }
+  if (items.length === 1) {
+    return itemRenderer(items[0]);
+  }
 
-    if (items.length === 1) {
-      return itemRenderer(items[0]);
-    }
-
-    return (
-      <div
-        className={classNames("sortable-table__multi-item-collapse", {
-          "is-open": open,
-        })}
-      >
-        <div className="sortable-table__multi-item-collapse_header">
-          <span className="sortable-table__multi-item-collapse_header_title">
-            {itemRenderer(items[0])}
-          </span>
-          <div className="sortable-table__multi-item-collapse_header_arrow-wrapper">
+  return (
+    <div
+      className={classNames("sortable-table__multi-item-collapse", {
+        "is-open": open,
+      })}
+    >
+      <div className="sortable-table__multi-item-collapse_header">
+        <span className="sortable-table__multi-item-collapse_header_title">
+          {itemRenderer(items[0])}
+        </span>
+        <div className="sortable-table__multi-item-collapse_header_arrow-wrapper">
+          {useHdsTagForCount ? (
+            <Tag size={TagSize.Small}>{String(items.length)}</Tag>
+          ) : (
             <span>({items.length})</span>
-          </div>
-        </div>
-        <div className="sortable-table__multi-item-collapse_items">
-          {items.map((item, index) => {
-            if (!index) return null;
-            return <ListItem key={index}>{itemRenderer(item)}</ListItem>;
-          })}
+          )}
         </div>
       </div>
-    );
-  }
-}
+      <div className="sortable-table__multi-item-collapse_items">
+        {items.map((item, index) => {
+          if (!index) return null;
+          return <ListItem key={index}>{itemRenderer(item)}</ListItem>;
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default MultiItemCollapse;
+export default memo(MultiItemCollapse);
