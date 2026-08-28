10e6cc1807c9b1d75dc2dc4528f722a40a5e792c
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-08-28 11:45:47 +0300

    fix issue of syncing `search` queryparam

--- apps/leasing/src/leases/components/LeaseListPage.tsx
+++ apps/leasing/src/leases/components/LeaseListPage.tsx
@@ -484,6 +484,10 @@ const LeaseListPage: React.FC = () => {
       delete searchQuery.preparer;
     }
 
+    if (query.search && !formValues.search) {
+      searchQuery.search = query.search;
+    }
+
     if (query.lease_state && !formValues.lease_state) {
       searchQuery.lease_state = query.lease_state;
     }
