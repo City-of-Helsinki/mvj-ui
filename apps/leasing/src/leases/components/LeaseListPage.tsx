fa7009100bc988a0557890a8e4fe84d289e64739
Juho Kettunen <juho.kettunen@nordcloud.com>
2026-08-31 16:47:40 +0300

    Allow fully deleting the search parameter
    This follow the expected behavior: if user deletes all text in the
    search box, the system should then do a search without the search
    parameter, giving full results (according to filters).

--- apps/leasing/src/leases/components/LeaseListPage.tsx
+++ apps/leasing/src/leases/components/LeaseListPage.tsx
@@ -484,10 +484,6 @@ const LeaseListPage: React.FC = () => {
       delete searchQuery.preparer;
     }
 
-    if (query.search && !formValues.search) {
-      searchQuery.search = query.search;
-    }
-
     if (query.lease_state && !formValues.lease_state) {
       searchQuery.lease_state = query.lease_state;
     }
